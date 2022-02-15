d5084818c0848facfb78f26dc992ecf2e208f9d4
devzonayed <zonayedofficial@gmail.com>
2022-02-15 21:48:03 +0600

    little change 2

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-let qc = document.querySelectorAll('.qcblock');
-let inner = document.querySelectorAll('.accordioninner li');
+const acc_btns = document.querySelectorAll('.accordion-header');
+const acc_content = document.querySelectorAll('.accordion-body');
 
-qc.forEach((element) => {
-    element.onclick = function(){
-        this.parentElement.classList.toggle('active')
-        if(this.parentElement.classList.contains('active')){
-            this.nextElementSibling.style.height = '100px'
-        }else{
-            this.nextElementSibling.style.height = '0px'
-        }
-    }
+acc_btns.forEach(btn => {
+    console.log(btn);
+    btn.addEventListener('click', (e) => {
+
+        acc_content.forEach(item => {
+            if(e.target.nextElementSibling != item && item.classList.contains('active')){
+                item.classList.remove('active')
+                acc_btns.forEach(btn => {btn.classList.remove('active')})
+            }
+        })
+
+        btn.classList.toggle('active')
+        btn.nextElementSibling.classList.toggle('active')
+
+    })
 })
